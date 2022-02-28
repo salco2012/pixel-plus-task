e543af6267d0cee45739f7f57f7b71ea9a666913
Igor Solodovnick <info@pro-landing.site>
2022-02-28 18:46:28 +0300

    видоизменил первоначальное состояние доски

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 function chessGenerate() {
   const board = document.querySelector('.board');
 
-  for (let i = 0; i < 8; i++) {
-       
-      for (let j = 0; j < 8; j++) {
-         board.innerHTML += `<div class="cell-new"></div>`;
-      }
-  }
+  let numCell = 0;
 
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (numCell % 2 === 0) {
+        board.innerHTML += `<div class="cell-new light"></div>`;
+      } else {
+        board.innerHTML += `<div class="cell-new black"></div>`;
+      }
+      numCell++;
+    }
+    numCell++;
+  }
 }
 
 chessGenerate();
